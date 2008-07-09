9b36c8bd2dfb00341e487f84537f92f551e937fc
joseph <joseph@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2008-07-09 00:12:33 +0000

    * sysdeps/powerpc/nofpu/shlib-versions: New. 	* c++-types-powerpcsoft-linux-gnu.data: New. 	* localplt-powerpcsoft-linux-gnu.data: New. 	* sysdeps/powerpc/nofpu/feholdexcpt.c (feholdexcept): Use 	__fegetenv.
    git-svn-id: svn://svn.eglibc.org/trunk@6518 7b3dc134-2b1b-0410-93df-9e9f96275f8d

--- ports/sysdeps/powerpc/nofpu/feholdexcpt.c
+++ ports/sysdeps/powerpc/nofpu/feholdexcpt.c
@@ -28,7 +28,7 @@ feholdexcept (fenv_t *envp)
   fenv_union_t u;
 
   /* Get the current state.  */
-  fegetenv (envp);
+  __fegetenv (envp);
 
   u.fenv = *envp;
   /* Clear everything except the rounding mode.  */
