447fd9179ef3effda7c99168c310c5e0d0e8e770
joseph <joseph@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2008-08-19 17:24:09 +0000

    Merge changes between r6509 and r6780 from /fsf/trunk.
    git-svn-id: svn://svn.eglibc.org/trunk@6785 7b3dc134-2b1b-0410-93df-9e9f96275f8d

--- ports/sysdeps/mips/fpu_control.h
+++ ports/sysdeps/mips/fpu_control.h
@@ -1,5 +1,6 @@
 /* FPU control word bits.  Mips version.
-   Copyright (C) 1996, 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2006, 2008
+   Free Software Foundation, Inc.
    This file is part of the GNU C Library.
    Contributed by Olaf Flebbe and Ralf Baechle.
 
