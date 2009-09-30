7ae8efaf2900de04894ce21f75e40031639c505c
joseph <joseph@7b3dc134-2b1b-0410-93df-9e9f96275f8d>
2009-09-30 15:40:23 +0000

    Merge changes between r8915 and r9028 from /fsf/trunk.
    git-svn-id: svn://svn.eglibc.org/trunk@9029 7b3dc134-2b1b-0410-93df-9e9f96275f8d

--- ports/sysdeps/unix/sysv/linux/hppa/nptl/pthread_cond_broadcast.c
+++ ports/sysdeps/unix/sysv/linux/hppa/nptl/pthread_cond_broadcast.c
@@ -1,7 +1,6 @@
-/* Create a directory named relative to another open directory.  Hurd version.
-   Copyright (C) 1991,1993,1994,1995,1996,1997,2002,2006
-	Free Software Foundation, Inc.
+/* Copyright (C) 2009 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
+   Contributed by Carlos O'Donell <carlos@codesourcery.com>, 2009.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -18,26 +17,27 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
-#include <errno.h>
-#include <stddef.h>
-#include <sys/stat.h>
-#include <hurd.h>
-#include <hurd/fd.h>
-
+#ifndef INCLUDED_SELF
+# define INCLUDED_SELF
+# include <pthread_cond_broadcast.c>
+#else
+# include <pthread.h>
+# include <pthreadP.h>
+# include <internaltypes.h>
+# include <shlib-compat.h>
 int
-mkdirat (fd, path, mode)
-     int fd;
-     const char *path;
-     mode_t mode;
+__pthread_cond_broadcast (cond)
+     pthread_cond_t *cond;
 {
-  error_t err;
-  const char *name;
-  file_t parent = __directory_name_split_at (fd, path, (char **) &name);
-  if (parent == MACH_PORT_NULL)
-    return -1;
-  err = __dir_mkdir (parent, name, mode & ~_hurd_umask);
-  __mach_port_deallocate (__mach_task_self (), parent);
-  if (err)
-    return __hurd_fail (err);
-  return 0;
+  cond_compat_check_and_clear (cond);
+  return __pthread_cond_broadcast_internal (cond);
 }
+versioned_symbol (libpthread, __pthread_cond_broadcast, pthread_cond_broadcast,
+                  GLIBC_2_3_2);
+# undef versioned_symbol
+# define versioned_symbol(lib, local, symbol, version)
+# undef __pthread_cond_broadcast
+# define __pthread_cond_broadcast __pthread_cond_broadcast_internal
+# include_next <pthread_cond_broadcast.c>
+#endif
+
--- ports/sysdeps/unix/sysv/linux/hppa/nptl/pthread_cond_destroy.c
+++ ports/sysdeps/unix/sysv/linux/hppa/nptl/pthread_cond_destroy.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 2009 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
-   Contributed by Jakub Jelinek <jakub@redhat.com>.
+   Contributed by Carlos O'Donell <carlos@codesourcery.com>, 2009.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -17,25 +17,27 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
-#include <errno.h>
-#include <sysdep.h>
-#include <setjmp.h>
-#include <bits/setjmp.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#ifndef INCLUDED_SELF
+# define INCLUDED_SELF
+# include <pthread_cond_destroy.c>
+#else
+# include <pthread.h>
+# include <pthreadP.h>
+# include <internaltypes.h>
+# include <shlib-compat.h>
+int
+__pthread_cond_destroy (cond)
+     pthread_cond_t *cond;
+{
+  cond_compat_check_and_clear (cond);
+  return __pthread_cond_destroy_internal (cond);
+}
+versioned_symbol (libpthread, __pthread_cond_destroy, pthread_cond_destroy,
+                  GLIBC_2_3_2);
+# undef versioned_symbol
+# define versioned_symbol(lib, local, symbol, version)
+# undef __pthread_cond_destroy
+# define __pthread_cond_destroy __pthread_cond_destroy_internal
+# include_next <pthread_cond_destroy.c>
+#endif
 
-#define __longjmp ____longjmp_chk
-
-#define CHECK_SP(env, guard) \
-  do									\
-    {									\
-      uintptr_t cur_sp;							\
-      uintptr_t new_sp = env->__gregs[9];				\
-      __asm ("lr %0, %%r15" : "=r" (cur_sp));				\
-      new_sp ^= guard;							\
-      if (new_sp < cur_sp)						\
-	__fortify_fail ("longjmp causes uninitialized stack frame");	\
-    } while (0)
-
-#include "__longjmp.c"
